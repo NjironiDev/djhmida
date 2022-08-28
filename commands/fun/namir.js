242d56cc5d122191c9e63b30d5f894e49d1147b7
ahmedbalady <baladyahmed19@gmail.com>
2022-08-28 12:31:10 +0100

    fix: djnamir, djmajid

--- commands/fun/namir.js
+++ commands/fun/namir.js
@@ -1,51 +1,36 @@
-const Commando = require("discord.js-commando");
+const Commando = require('discord.js-commando');
 
 const voices = {
-  wach_kayn:
-    "https://cdn.discordapp.com/attachments/1008058197141106699/1011749023989911612/Wach_kayn.mp3",
-  zamel:
-    "https://cdn.discordapp.com/attachments/849299865157632098/891398001510219777/namir.mp3",
-  da7kek:
-    "https://cdn.discordapp.com/attachments/596120108334579728/1010961830019076196/namir_da7kek.mp3",
-  rak3yan:
-    "https://cdn.discordapp.com/attachments/596120108334579728/1010961830396575764/namir_rak3yan.mp3",
-  aller:
-    "https://cdn.discordapp.com/attachments/596120108334579728/1010961830732103800/namir_Aller.mp3",
-  li_ma3na:
-    "https://cdn.discordapp.com/attachments/596120108334579728/1010961831042494474/namir_li_ma3na.mp3",
-  salam:
-    "https://cdn.discordapp.com/attachments/596120108334579728/1010961831378034768/namirSalam.mp3",
-  wooo: "https://cdn.discordapp.com/attachments/596120108334579728/1010961831831031809/namir_wooo.mp3",
-  hehe: "https://cdn.discordapp.com/attachments/1005814495987388459/1010155223794327562/namir-hehe.mp3",
+  wach_kayn: 'https://cdn.discordapp.com/attachments/1008058197141106699/1011749023989911612/Wach_kayn.mp3',
+  zamel: 'https://cdn.discordapp.com/attachments/849299865157632098/891398001510219777/namir.mp3',
+  da7kek: 'https://cdn.discordapp.com/attachments/596120108334579728/1010961830019076196/namir_da7kek.mp3',
+  rak3yan: 'https://cdn.discordapp.com/attachments/596120108334579728/1010961830396575764/namir_rak3yan.mp3',
+  aller: 'https://cdn.discordapp.com/attachments/596120108334579728/1010961830732103800/namir_Aller.mp3',
+  li_ma3na: 'https://cdn.discordapp.com/attachments/596120108334579728/1010961831042494474/namir_li_ma3na.mp3',
+  salam: 'https://cdn.discordapp.com/attachments/596120108334579728/1010961831378034768/namirSalam.mp3',
+  hehe: 'https://cdn.discordapp.com/attachments/1005814495987388459/1010155223794327562/namir-hehe.mp3',
 };
 
 module.exports = class NamirCommand extends Commando.Command {
   constructor(client) {
     super(client, {
-      name: "namir",
-      group: "fun",
-      memberName: "namir",
-      description: 'Plays "wa namir *amel"',
+      name: 'namir',
+      group: 'fun',
+      memberName: 'namir',
+      description: 'Aswat mamir mo9ana3',
     });
   }
 
   async run(message, args) {
     const { voice } = message.member;
 
-    if (!voice.channelID)
-      return message.reply("You must be in a voice channel 🙊");
+    if (!voice.channelID) return message.reply('You must be in a voice channel 🙊');
 
-    if (!args)
-      return message.reply(
-        "Please supply an option `djnamir zamel|da7kek|rak3yan|aller|li_ma3na|salam|wooo|wach_kayn`"
-      );
+    if (!args) return message.reply(`Please supply an option \`${Object.keys(voices).join('|')}\``);
 
-    const [option] = args.split(" ");
+    const [option] = args.split(' ');
 
-    if (!Object.keys(voices).includes(option))
-      return message.reply(
-        "Option does not exist, use the given options `djnamir zamel|da7kek|rak3yan|aller|li_ma3na|salam|wooo|wach_kayn`"
-      );
+    if (!Object.keys(voices).includes(option)) return message.reply(`Option does not exist, use the given options \`${Object.keys(voices).join('|')}\``);
 
     try {
       const connection = await voice.channel.join();
--- commands/general/help.js
+++ commands/general/help.js
@@ -1,58 +1,54 @@
-const Commando = require("discord.js-commando");
-const Discord = require("discord.js");
+const Commando = require('discord.js-commando');
+const Discord = require('discord.js');
 
 module.exports = class HelpCommand extends Commando.Command {
   constructor(client) {
     super(client, {
-      name: "help",
-      group: "general",
-      memberName: "general",
-      description: "Displays commands help",
+      name: 'help',
+      group: 'general',
+      memberName: 'general',
+      description: 'Displays commands help',
     });
   }
 
   async run(message) {
     const embed = new Discord.MessageEmbed()
-      .setColor("RANDOM")
-      .setTitle("DJ Hmida ♫♪ Command List")
+      .setColor('RANDOM')
+      .setTitle('DJ Hmida ♫♪ Command List')
       .addFields(
         { name: `djadvice 🐢`, value: "Get Oogway's wisdom.", inline: true },
         {
           name: `djanime 😳 <anime>`,
-          value: "Search for anime.",
+          value: 'Search for anime.',
           inline: true,
         },
-        { name: `djbbquote ⚗`, value: "Breaking Bad quote.", inline: true },
-        { name: `djcat 🐱`, value: "Random cute cat.", inline: true },
-        { name: `djface 👩`, value: "Random face.", inline: true },
-        { name: `djhelp 🆘`, value: "Show help.", inline: true },
-        { name: `djmeme 🌈`, value: "Random meme.", inline: true },
-        { name: `djping 🏓`, value: "Show your ping.", inline: true },
-        { name: `djspawn 🙋‍♂️`, value: "Spawn your friends.", inline: true },
+        { name: `djbbquote ⚗`, value: 'Breaking Bad quote.', inline: true },
+        { name: `djcat 🐱`, value: 'Random cute cat.', inline: true },
+        { name: `djface 👩`, value: 'Random face.', inline: true },
+        { name: `djhelp 🆘`, value: 'Show help.', inline: true },
+        { name: `djmeme 🌈`, value: 'Random meme.', inline: true },
+        { name: `djping 🏓`, value: 'Show your ping.', inline: true },
+        { name: `djspawn 🙋‍♂️`, value: 'Spawn your friends.', inline: true },
         {
           name: `djurban 📚 <word>`,
-          value: "Search in the urban dictionnary.",
+          value: 'Search in the urban dictionnary.',
           inline: true,
         },
         {
           name: `djweather 🌡 <city>`,
-          value: "Weather for the given city",
+          value: 'Weather for the given city',
           inline: true,
         },
         {
           name: `djnamir 🐅 <option>`,
-          value:
-            "Choose one of these options and hear 👂🏻 zamel|da7kek|rak3yan|aller|li_ma3na|salam|wooo.",
-          inline: true,
+          value: 'Choose one of these options and hear 👂🏻 zamel|da7kek|rak3yan|aller|li_ma3na|salam|wooo.',
         },
         {
           name: `djmajid ⚽ <option>`,
-          value:
-            "Choose one of these options and hear 👂🏻 dik_terma|ana_ghadi|mabaghinch_tmcho|hak_a_said|hak_chwa|ay_dahri|bezi|ara_passi|l3eb_m3ana.",
-          inline: true,
-        }
+          value: 'Choose one of these options and hear 👂🏻 dik_terma|ana_ghadi|mabaghinch_tmcho|hak_a_said|hak_chwa|ay_dahri|bezi|ara_passi|l3eb_m3ana.',
+        },
       )
-      .setFooter("Keep vibing 🎶🎵");
+      .setFooter('Keep vibing 🎶🎵');
 
     message.channel.send({ embed });
   }
